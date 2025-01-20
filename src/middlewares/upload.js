57950c4cc2adeeace6a9e97b9ae28a3f65dd5c52
kaktuz <120398316+evgenyaroviy@users.noreply.github.com>
2025-01-20 20:25:29 +0200

    Update upload.js

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -5,24 +5,26 @@ import { TEMP_UPLOAD_DIR } from "../constants/index.js";
 const storage = multer.diskStorage({
     destination: TEMP_UPLOAD_DIR,
     filename: (req, file, cb) => {
-        const uniquePreffix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`;
-        const filename = `${uniquePreffix}_${file.originalname}`;
+        const uniquePrefix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`; 
+        const filename = `${uniquePrefix}_${file.originalname}`;
         cb(null, filename);
     },
 });
     
-const limit = {
+const limits = { 
     fileSize: 5 * 1024 * 1024,
 };
 
 const fileFilter = (req, file, cb) => {
-    const extantion = file.originalname.split('.').pop();
-    if(extantion === 'exe') {
-        return cb(new Error(400, 'exe files are not allowed'));
+    const extension = file.originalname.split('.').pop(); 
+    if (extension === 'exe') {
+        return cb(new Error('exe files are not allowed'), false); 
     }
+    cb(null, true); 
 };
+
 export const upload = multer({
     storage,
     fileFilter,
-    limit,
+    limits, 
 });
